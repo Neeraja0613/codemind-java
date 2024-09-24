dcbfbbeec2a999a6ec4cc62627841377e8dbd4d5
Neeraja0613 <147791902+Neeraja0613@users.noreply.github.com>
2024-09-25 00:11:11 +0530

    import java.util.Scanner

---
+++ charArrayForEachLoop.java
@@ -0,0 +1,11 @@
+public class StringExample4 {
+    public static void main(String[] args)
+    {
+        String s= "Aditya engineering colege";
+        // for-each loop on a string
+        for(char each: s.toCharArray())
+        {
+            System.out.println(each);
+        }
+    }
+}
