7ff960f3f74dddfe65c6b4266d0a31d4e2974fe4
Neeraja0613 <147791902+Neeraja0613@users.noreply.github.com>
2024-11-24 12:30:10 +0530

    import java.util.Scanner

---
+++ stringMethods.java
@@ -0,0 +1,13 @@
+public class StringExample {
+    public static void main(String[] args) {
+        String str1 = "Hello";
+        String str2 = "Hello";
+        
+        // Using String methods
+        System.out.println(str1.length());  // Output: 5
+        System.out.println(str1.charAt(1)); // Output: e
+        
+        // String comparison
+        System.out.println(str1.equals(str2)); // Output: true
+    }
+}
