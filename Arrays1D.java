bf52702aa5358ca299ad96dc1878c1c7ac62a3bd
Neeraja0613 <147791902+Neeraja0613@users.noreply.github.com>
2024-12-30 20:32:34 +0530

    import java.util.Scanner

---
+++ Arrays1D.java
@@ -0,0 +1,10 @@
+public class OneDimensionalArray {
+    public static void main(String[] args) {
+        // Declaring and initializing the array
+        int[] numbers = {10, 20, 30, 40, 50};
+        
+        // Accessing array elements
+        System.out.println(numbers[0]); // First element
+        System.out.println(numbers[1]); // Second element
+    }
+}
