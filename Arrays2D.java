fd0e49f46e005168c209870db1d0f4ef9127385c
Neeraja0613 <147791902+Neeraja0613@users.noreply.github.com>
2024-12-31 18:57:29 +0530

    import java.util.Scanner

---
+++ Arrays2D.java
@@ -0,0 +1,14 @@
+public class TwoDimensionalArray {
+    public static void main(String[] args) {
+        // Declaring and initializing the 2D array
+        int[][] matrix = {
+            {1, 2},
+            {3, 4},
+            {5, 6}
+        };
+
+        // Accessing array elements
+        System.out.println(matrix[0][0]); // Element at row 1, column 1
+        System.out.println(matrix[1][1]); // Element at row 2, column 2
+    }
+}
